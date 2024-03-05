629345b081e243ac01870e1eaf7840b334af3e6b
Lovel George <lovelgeorge22@gmail.com>
2024-03-05 09:58:17 -0500

    adeed insta link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,8 @@ function Footer() {
         <a href='https://github.com/lovelgeorge99' target='_blank'><ion-icon name="logo-github"></ion-icon></a>
         </div>
         <div className='text-5xl text-white m-3 hover:text-pink-500'>
-        <ion-icon name="logo-instagram"></ion-icon>
+        <a href='https://www.instagram.com/lovel_george/' target='_blank'><ion-icon name="logo-instagram"></ion-icon></a>
+        
         </div>
         <div className='text-5xl  m-3 text-white hover:text-blue-400'>
         <a href='https://www.linkedin.com/in/lovel-george/' target='_blank'><ion-icon name="logo-linkedin"></ion-icon></a>
